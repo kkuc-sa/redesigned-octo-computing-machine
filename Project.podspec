b9b099fc2b5acecc5c86af23e922de6cd58b9300
Krzysztof Kuc <krzysztof.kuc@summer.agency>
2023-09-11 11:49:54 +0200

    Add cocoapods-related scripts

---
+++ Project.podspec
@@ -0,0 +1,21 @@
+Pod::Spec.new do |s|
+
+    s.name         = "Project"
+    s.version      = "0.3.1"
+    s.summary      = "Project for iOS"
+  
+    s.description  = <<-DESC
+    Project framework
+                      DESC
+  
+    s.homepage     = "https://github.com/kkuc-sa/redesigned-octo-computing-machine.git"
+    s.license      = "MIT"
+    s.author       = "kkuc-sa"
+  
+    s.platform = :ios, "16.0"
+  
+    s.source = { :http => "https://github.com/kkuc-sa/redesigned-octo-computing-machine/releases/download/0.0.9/Project.xcframework.zip" }
+    s.vendored_frameworks = "Project.xcframework"
+  
+  end
+    
---
+++ fastlane/actions/set_podspec_http_source.rb
@@ -0,0 +1,40 @@
+module Fastlane
+    module Actions
+      class SetPodspecHttpSourceAction < Action
+        def self.run(params)
+          sh("sed -i '' 's|s.source =.*|s.source = { :http => \"#{params[:binary_zip_path]}\" }|g' #{params[:podspec_path]}")
+        end
+  
+        #####################################################
+        # @!group Documentation
+        #####################################################
+  
+        def self.description
+          "Replaces podspec source http path"
+        end
+  
+        def self.available_options
+          [
+            FastlaneCore::ConfigItem.new(key: :binary_zip_path,
+                                         description: 'Path to binary zip file',
+                                         optional: false,
+                                         is_string: true),
+  
+            FastlaneCore::ConfigItem.new(key: :podspec_path,
+                                         description: 'Path to podspec file',
+                                         optional: false,
+                                         is_string: true)
+          ]
+        end
+  
+        def self.authors
+          ["kkuc-sa"]
+        end
+  
+        def self.is_supported?(platform)
+          platform == :ios
+        end
+      end
+    end
+  end
+  
